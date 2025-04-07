da3e912616c67a1a54ce5347bc6eebae14376899
UkhyunGo <dnrwk99@gmail.com>
2025-04-07 18:09:32 +0900

    feat: Input + Output 구현

--- NaeilBurgerCamp/NaeilBurgerCamp/Presentation/Main/MainViewModel.swift
+++ NaeilBurgerCamp/NaeilBurgerCamp/Presentation/Main/MainViewModel.swift
@@ -1,7 +1,26 @@
-//
-//  MainViewModel.swift
-//  NaeilBurgerCamp
-//
-//  Created by youseokhwan on 4/7/25.
-//
+import Foundation
+import RxSwift
+import RxCocoa
 
+class MainViewModel {
+    
+    private let disposeBag = DisposeBag()
+    
+    struct Input {
+        let upperMenuButtonTap: Observable<Void>
+        let cellInteraction: Observable<Item>
+        let increaseQuantityButtonTap: Observable<Void>
+        let decreaseQuantityButtonTap: Observable<Void>
+        let cancelButtonTap: Observable<Void>
+        let orderButtonTap: Observable<Void>
+    }
+    
+    struct Output {
+        let upperMenuAction: Observable<Void>
+        let cellInteractionResult: Observable<Item>
+        let updateQuantity: Observable<[Item]>
+        let cancelResult: Observable<Void>
+        let orderCompletion: Observable<Result<Void, Error>>
+    }
+
+}
