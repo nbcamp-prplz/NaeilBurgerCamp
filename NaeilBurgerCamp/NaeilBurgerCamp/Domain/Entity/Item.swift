9ce5d846613cbf64e522fe95749275c3155d6dc2
LeeSuHyeon <tngus0673@naver.com>
2025-04-07 17:02:39 +0900

    feat: Item Entity 구현

---
+++ NaeilBurgerCamp/NaeilBurgerCamp/Domain/Entity/Item.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+struct Item: Decodable {
+    let id: Int
+    let category: String // (단품, 사이드, 음료)
+    let title: String
+    let imageURL: String
+    let price: Int
+}
+/*
+ etc)
+ - Calorie: Int
+ - isNew: Bool
+ - isBest: Bool
+ - isOutOfStock: Bool
+ */
