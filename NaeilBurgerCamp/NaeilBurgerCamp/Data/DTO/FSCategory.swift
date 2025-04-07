fed70efb9f6417b7e67f55488d0169a36a147fc8
youseokhwan <youseokhwan15@gmail.com>
2025-04-08 00:09:11 +0900

    feat: FirestoreService fetchCategories 구현

---
+++ NaeilBurgerCamp/NaeilBurgerCamp/Data/DTO/FSCategory.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct FSCategories: Decodable {
+    let documents: [FSCategoryDocument]
+}
+
+struct FSCategoryDocument: Decodable {
+    let name: String
+    let fields: FSCategory
+}
+
+struct FSCategory: Decodable {
+    let id: FSString
+    let title: FSString
+}
