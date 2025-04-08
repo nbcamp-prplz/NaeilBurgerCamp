5612ad7c7c9d2826e0dd39a1d4489a5995b49d59
UkhyunGo <dnrwk99@gmail.com>
2025-04-08 23:36:34 +0900

    feat: Input & Output 초기 구조 구현

--- NaeilBurgerCamp/NaeilBurgerCamp/Presentation/Main/MainViewModel.swift
+++ NaeilBurgerCamp/NaeilBurgerCamp/Presentation/Main/MainViewModel.swift
@@ -6,17 +6,29 @@ protocol MainViewModelProtocol: AnyObject {
     associatedtype Input
     associatedtype Output
     
-    var output: Output { get }
-    
-    func transform(input: Input)
+    func transform(input: Input) -> Output
 }
 
 final class MainViewModel: MainViewModelProtocol {
     private let disposeBag = DisposeBag()
-    var output = Output()
     
-    struct Input { }
-    struct Output { }
-    
-    func transform(input: Input) { }
+    struct Input {
+        let categorySelected: Observable<Void>
+        let menuItemSelected: Observable<Void>
+        let increaseQuantityTapped: Observable<Void>
+        let decreaseQuantityTapped: Observable<Void>
+        let cancelTapped: Observable<Void>
+        let orderTapped: Observable<Void>
+    }
+    struct Output {
+        let orders = PublishRelay<Void>()
+        let cancelResult = PublishRelay<Void>()
+        let items = BehaviorRelay<[Item]>(value: [])
+    }
+
+    func transform(input: Input) -> Output {
+        var output = Output()
+        return output
+    }
 }
+
