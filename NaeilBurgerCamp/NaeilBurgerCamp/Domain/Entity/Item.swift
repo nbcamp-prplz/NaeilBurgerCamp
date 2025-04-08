a75b6322236d9808c19d35e6a7f3d161518b453d
LeeSuHyeon <tngus0673@naver.com>
2025-04-08 11:26:54 +0900

    refactor: 필요없는 주석 제거

--- NaeilBurgerCamp/NaeilBurgerCamp/Domain/Entity/Item.swift
+++ NaeilBurgerCamp/NaeilBurgerCamp/Domain/Entity/Item.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct MenuItem {
     let id: Int
-    let categoryID: String // (단품, 사이드, 음료)
+    let categoryID: String
     let title: String
     let price: Int
     let imageURL: String
