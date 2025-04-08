e2088322925f10835cf78c3fcb277279d813a764
LeeSuHyeon <tngus0673@naver.com>
2025-04-08 19:33:40 +0900

    feat: MenuUseCase 구현

---
+++ NaeilBurgerCamp/NaeilBurgerCamp/Domain/UseCase/MenuUseCase.swift
@@ -0,0 +1,27 @@
+import Foundation
+import RxSwift
+
+protocol MenuUseCaseProtocol {
+    func fetchCategories()
+    func fetchMenuItems(categoryID: String)
+    var categories: BehaviorSubject<Categories> { get }
+    var menuItems: BehaviorSubject<MenuItems> { get }
+}
+
+class MenuUseCase: MenuUseCaseProtocol {
+    private let repository: MenuRepositoryProtocol
+    let categories = BehaviorSubject<Categories>(value: [])
+    let menuItems = BehaviorSubject<MenuItems>(value: [])
+
+    init(repository: MenuRepositoryProtocol) {
+        self.repository = repository
+    }
+
+    func fetchCategories() {
+        categories.onNext(repository.fetchCategories())
+    }
+    
+    func fetchMenuItems(categoryID: String)  {
+        menuItems.onNext(repository.fetchMenuItems(categoryID: categoryID))
+    }
+}
