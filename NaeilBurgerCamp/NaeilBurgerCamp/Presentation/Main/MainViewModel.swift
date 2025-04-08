b34b048134bd20a9437e5ae8e559177654e286ae
UkhyunGo <dnrwk99@gmail.com>
2025-04-08 09:51:15 +0900

    feat: protocol & 기본 구조 설정

--- NaeilBurgerCamp/NaeilBurgerCamp/Presentation/Main/MainViewModel.swift
+++ NaeilBurgerCamp/NaeilBurgerCamp/Presentation/Main/MainViewModel.swift
@@ -2,32 +2,21 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-protocol MainViewModelProtocol {
-    func transform(input: MainViewModel.Input) -> MainViewModel.Output
+protocol MainViewModelProtocol: AnyObject {
+    associatedtype Input
+    associatedtype Output
+    var output: Output { get }
+    func transform(input: Input)
 }
 
-class MainViewModel: MainViewModelProtocol {
-    
+final class MainViewModel: MainViewModelProtocol {
     private let disposeBag = DisposeBag()
+    var output: Output
     
-    struct Input {
-        let upperMenuButtonTap: Observable<Void>
-        let cellInteraction: Observable<Item>
-        let increaseQuantityButtonTap: Observable<Void>
-        let decreaseQuantityButtonTap: Observable<Void>
-        let cancelButtonTap: Observable<Void>
-        let orderButtonTap: Observable<Void>
-    }
+    struct Input {}
+    struct Output {}
     
-    struct Output {
-        let upperMenuAction: Observable<Void>
-        let cellInteractionResult: Observable<Item>
-        let updateQuantity: Observable<[Item]>
-        let cancelResult: Observable<Void>
-        let orderCompletion: Observable<Result<Void, Error>>
-    }
-    
-    func transform(input: Input) -> Output {
-        <#code#>
-    }
+    func transform(input: Input) {}
 }
+
+
