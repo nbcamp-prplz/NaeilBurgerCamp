05ea8f3047c91262509f1ce000a29c2189cf84ac
UkhyunGo <dnrwk99@gmail.com>
2025-04-09 12:04:24 +0900

    feat: transform - orderTapped 구현 & order 결과 반환(Alert, Bool)

--- NaeilBurgerCamp/NaeilBurgerCamp/Presentation/Main/MainViewModel.swift
+++ NaeilBurgerCamp/NaeilBurgerCamp/Presentation/Main/MainViewModel.swift
@@ -9,15 +9,20 @@ protocol MainViewModelProtocol: AnyObject {
     func transform(input: Input) -> Output
 }
 
+struct DummyOrderDetails {
+    let menuItem: MenuItem
+    let quantity: Int
+}
+
 final class MainViewModel: MainViewModelProtocol {
     private let disposeBag = DisposeBag()
     private let selectedCategory = BehaviorRelay<Int>(value: 1)
-    private let selectedMenuItem = PublishRelay<Item>()
+    private let selectedMenuItem = BehaviorRelay<MenuItem?>(value: nil)
     private let quantity = BehaviorRelay<Int>(value: 0)
     
     struct Input {
         let categorySelected: Observable<Int>
-        let menuItemSelected: Observable<Item>
+        let menuItemSelected: Observable<MenuItem>
         let increaseQuantityTapped: Observable<Void>
         let decreaseQuantityTapped: Observable<Void>
         let cancelTapped: Observable<Void>
@@ -26,8 +31,9 @@ final class MainViewModel: MainViewModelProtocol {
     struct Output {
         let orders = PublishRelay<Void>()
         let cancelResult = PublishRelay<Void>()
-        let items = BehaviorRelay<[Item]>(value: [])
+        let items = BehaviorRelay<[MenuItem]>(value: [])
         let quantity: Observable<Int>
+        let orderResult = PublishRelay<Bool>()
     }
     
     func transform(input: Input) -> Output {
@@ -38,40 +44,55 @@ final class MainViewModel: MainViewModelProtocol {
             .disposed(by: disposeBag)
         
         input.menuItemSelected
-            .bind(to: selectedMenuItem)
+            .subscribe(onNext: { [weak self] menuItem in
+                self?.selectedMenuItem.accept(menuItem)
+            })
             .disposed(by: disposeBag)
         
         input.increaseQuantityTapped
             .subscribe(onNext: { [weak self] in
-                let currentQuantity = self?.quantity.value ?? 0
-                self?.quantity.accept(currentQuantity + 1)
+                guard let self = self else { return }
+                let currentQuantity = self.quantity.value
+                self.quantity.accept(currentQuantity + 1)
             })
             .disposed(by: disposeBag)
         
         input.decreaseQuantityTapped
             .subscribe(onNext: { [weak self] in
-                let currentQuantity = self?.quantity.value ?? 0
+                guard let self = self else { return }
+                let currentQuantity = self.quantity.value
                 if currentQuantity > 0 {
-                    self?.quantity.accept(currentQuantity - 1)
+                    self.quantity.accept(currentQuantity - 1)
                 }
             })
             .disposed(by: disposeBag)
         
         input.cancelTapped
             .subscribe(onNext: { [weak self] in
-                self?.quantity.accept(0)
+                guard let self = self else { return }
+                self.quantity.accept(0)
+                self.selectedMenuItem.accept(nil)
                 output.cancelResult.accept(())
             })
             .disposed(by: disposeBag)
         
         input.orderTapped
             .subscribe(onNext: { [weak self] in
-                
-                output.orders.accept(())
+                guard let self = self else { return }
+                guard let selectedMenuItem = self.selectedMenuItem.value else { return }
+                let orderDetails = DummyOrderDetails(menuItem: selectedMenuItem, quantity: self.quantity.value)
+                let isSuccess = self.placeOrder(orderDetails: orderDetails)
+                output.orderResult.accept(isSuccess)
+                if isSuccess {
+                    self.quantity.accept(0)
+                    self.selectedMenuItem.accept(nil)
+                }
             })
             .disposed(by: disposeBag)
-        
         return output
     }
+    private func placeOrder(orderDetails: DummyOrderDetails) -> Bool {
+            print("주문 처리: \(orderDetails.menuItem.title), 수량: \(orderDetails.quantity)")
+            return true
+        }
 }
-
