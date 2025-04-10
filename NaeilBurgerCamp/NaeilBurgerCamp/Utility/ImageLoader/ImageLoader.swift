39b847c8e34557367ef1e76285f6c1140c3bd5b0
LeeSuHyeon <tngus0673@naver.com>
2025-04-10 15:50:05 +0900

    refactor: print -> os_log로 변경

--- NaeilBurgerCamp/NaeilBurgerCamp/Utility/ImageLoader/ImageLoader.swift
+++ NaeilBurgerCamp/NaeilBurgerCamp/Utility/ImageLoader/ImageLoader.swift
@@ -1,44 +1,45 @@
 import Foundation
+import os
 
 final class ImageLoader {
     static let shared = ImageLoader()
-    
+
     private let cache = NSCache<NSString, NSData>()
     private let taskStore = OngoingTaskStore<Data>()
     private let expiryStore = ExpiryDateStore()
     private let ttl: TimeInterval = 5 * 60 // 5분
-    
+
     private init() {
         cache.totalCostLimit = 50 * 1024 * 1024 // 캐시 총 용량 제한 (50MB)
     }
-    
+
     func loadImageData(for urlString: String) async -> Data? {
         if let cachedImageData = cache.object(forKey: urlString as NSString),
            let expiryDate = await expiryStore.expiryDate(for: urlString) {
 
             if expiryDate > Date() {
-                print("캐시 이미지 사용")
+                os_log("[ImageLoader]: 캐시 이미지 사용", type: .default)
                 return cachedImageData as Data
             }
-            
+
             cache.removeObject(forKey: urlString as NSString)
         }
-        
+
         if let ongoingTask = await taskStore.task(for: urlString) {
             let image = await ongoingTask.value
-            print("중복 요청 방지 후 이미지 데이터 반환")
+            os_log("[ImageLoader]: 중복 요청 방지 후 이미지 데이터 반환", type: .default)
             return image
         }
-        
+
         let task = Task<Data?, Never> {
             defer {
                 Task { await taskStore.removeTask(for: urlString) }
             }
-            
+
             guard let url = URL(string: urlString) else { return nil }
-            
+
             do {
-                let (data, _ ) = try await URLSession.shared.data(from: url)
+                let (data, _) = try await URLSession.shared.data(from: url) //TODO: URLSession Error 처리
                 cache.setObject(
                     data as NSData,
                     forKey: urlString as NSString,
@@ -48,19 +49,20 @@ final class ImageLoader {
                     for: urlString,
                     to: Date().addingTimeInterval(ttl)
                 )
-                print("이미지 다운로드 성공")
+                os_log("[ImageLoader]: 이미지 다운로드 성공", type: .default)
                 return data
             } catch {
-                print("이미지 다운로드 실패")
+                os_log("[ImageLoader]: 이미지 다운로드 실패", type: .fault)
                 return nil
             }
         }
-        
+
         await taskStore.setTask(task, for: urlString)
         return await task.value
     }
-    
+
     func clearCache() async {
+        os_log("[ImageLoader]: 이미지 캐시 초기화", type: .default)
         cache.removeAllObjects()
         await taskStore.clear()
         await expiryStore.clear()
